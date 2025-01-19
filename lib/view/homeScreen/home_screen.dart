4cca6e441be14b340f562254be61b2fe4d0b19d1
Layheang <hak.layheang277@gmail.com>
2025-01-19 23:31:16 +0700

    Layheang change some Icon on bottomNavBar and save use icon from figma

--- lib/view/homeScreen/home_screen.dart
+++ lib/view/homeScreen/home_screen.dart
@@ -9,11 +9,9 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     ThemeData theme = Theme.of(context);
-    // return _buildBody(theme);
     return Scaffold(
       appBar: _buildAppBar(theme, context),
       body: _buildBody(theme),
-      // bottomNavigationBar: _buildFooter(theme, context),
     );
   }
 
@@ -33,7 +31,7 @@ class HomeScreen extends StatelessWidget {
           icon: Icon(themeLogic.mode == ThemeMode.dark
               ? Icons.dark_mode_outlined
               : Icons.light_mode_outlined),
-          iconSize: 30,
+          iconSize: 28,
           onPressed: () {
             // Toggle between light and dark mode
             if (themeLogic.mode == ThemeMode.dark) {
@@ -47,15 +45,16 @@ class HomeScreen extends StatelessWidget {
         IconButton(
           onPressed: () {},
           icon: const Icon(Icons.favorite_border_rounded),
-          iconSize: 30,
+          iconSize: 28,
         ),
         IconButton(
           onPressed: () {},
           icon: Image.asset(
             'lib/assets/fi_send.png',
             color: theme.appBarTheme.foregroundColor,
+            width: 25,
           ),
-          iconSize: 30,
+          iconSize: 20,
         ),
       ],
     );
@@ -71,60 +70,4 @@ class HomeScreen extends StatelessWidget {
       ),
     );
   }
-
-  // Widget _buildFooter(ThemeData theme, BuildContext context) {
-  //   final themeLogic = Provider.of<ThemeLogic>(context);
-  //
-  //   return BottomAppBar(
-  //     color: theme.appBarTheme.backgroundColor,
-  //     height: 70,
-  //     clipBehavior: Clip.antiAlias,
-  //     child: Container(
-  //       decoration: BoxDecoration(
-  //         border: Border(
-  //           top: BorderSide(
-  //             color: themeLogic.mode == ThemeMode.dark
-  //                 ? Colors.grey.shade800
-  //                 : Colors.grey.shade200,
-  //           ),
-  //         ),
-  //       ),
-  //       child: Row(
-  //         mainAxisAlignment: MainAxisAlignment.spaceBetween,
-  //         children: [
-  //           IconButton(
-  //             onPressed: () {},
-  //             icon: const Icon(Icons.home_filled),
-  //             iconSize: 30,
-  //             color: theme.appBarTheme.foregroundColor,
-  //           ),
-  //           IconButton(
-  //             onPressed: () {},
-  //             icon: const Icon(Icons.search),
-  //             iconSize: 30,
-  //             color: theme.appBarTheme.foregroundColor,
-  //           ),
-  //           IconButton(
-  //             onPressed: () {},
-  //             icon: const Icon(Icons.add),
-  //             iconSize: 30,
-  //             color: theme.appBarTheme.foregroundColor,
-  //           ),
-  //           IconButton(
-  //             onPressed: () {},
-  //             icon: const Icon(Icons.slow_motion_video_sharp),
-  //             iconSize: 30,
-  //             color: theme.appBarTheme.foregroundColor,
-  //           ),
-  //           IconButton(
-  //             onPressed: () {},
-  //             icon: const Icon(Icons.account_circle),
-  //             iconSize: 30,
-  //             color: theme.appBarTheme.foregroundColor,
-  //           ),
-  //         ],
-  //       ),
-  //     ),
-  //   );
-  // }
 }
